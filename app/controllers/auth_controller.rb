bc4e3a136442c6902b1b4f810f23bd4d01bd775b
Marcos Velasco <devslife7@gmail.com>
2022-09-03 21:46:09 -0400

    added a status: :unauthorized to login return

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,26 +3,27 @@ class AuthController < ApplicationController
 
   def login
     user = User.find_by(username: login_params[:username])
-      if user && user.authenticate(login_params[:password])
-        token = encode_token({ user_id: user.id })
-        
-        render json: { user: user, token: token },
-          except: [:created_at, :updated_at, :password_digest],
-          include: [
-            :invitations,
-            :watchparties,
-            :friends => {
-              except: [:created_at, :updated_at, :password_digest]
-            },
-            :user_leagues => {
-              only: [:id],
-              include: [
-                :league => { except: [:created_at, :updated_at] }
-              ]
-            }]
-      else
-        render json: { error: 'Invalid username or password' }
-      end
+
+    if user && user.authenticate(login_params[:password])
+      token = encode_token({ user_id: user.id })
+      
+      render json: { user: user, token: token },
+        except: [:created_at, :updated_at, :password_digest],
+        include: [
+          :invitations,
+          :watchparties,
+          :friends => {
+            except: [:created_at, :updated_at, :password_digest]
+          },
+          :user_leagues => {
+            only: [:id],
+            include: [
+              :league => { except: [:created_at, :updated_at] }
+            ]
+          }]
+    else
+      render json: { error: 'Invalid username or password' }, status: :unauthorized
+    end
   end
 
   # End point to wake up Heroku
